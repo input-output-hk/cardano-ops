08335c48e375869f861e3b16d76815e67e66ea22
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2019-11-26 16:21:48 +0100

    Add globals and topology for testnet

---
+++ globals-testnet.nix
@@ -0,0 +1,25 @@
+self: super: {
+  globals = import ./globals-defaults.nix // rec {
+
+    static = import ./static;
+
+    deploymentName = "rc-staging";
+
+    domain = "cardano-testnet.iohkdev.io";
+
+    configurationKey = "testnet_full";
+
+    environment = "testnet";
+
+    topology = import ./topologies/testnet.nix;
+
+    ec2 = {
+      credentials = {
+        accessKeyIds = {
+          "IOHK" = "default";
+          dns = "default";
+        };
+      };
+    };
+  };
+}
