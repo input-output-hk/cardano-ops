e5c825a2ec2e8e9ebbc1e7223b514dd95c554c46
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2019-11-26 11:56:18 +0100

    Use deployment.keys for core nodes keys.

--- roles/core.nix
+++ roles/core.nix
@@ -1,8 +1,42 @@
 
-{
+{config, name, lib, ...}:
+let
+  nodeId = config.node.nodeId;
+  leftPad = number: width: lib.fixedWidthString width "0" (toString number);
+  signingKey = ../keys/delegate-keys + ".${leftPad nodeId 3}.key";
+  delegationCertificate = ../keys/delegation-cert + ".${leftPad nodeId 3}.json";
+
+in {
 
   imports = [
     ../modules/base-service.nix
   ];
+
+  services.cardano-node = {
+    signingKey = "/var/lib/keys/cardano-node-signing";
+    delegationCertificate = "/var/lib/keys/cardano-node-delegation-cert";
+  };
+
+  systemd.services."cardano-node" = {
+    after = [ "cardano-node-signing-key.service" "cardano-node-delegation-cert-key.service" ];
+    wants = [ "cardano-node-signing-key.service" "cardano-node-delegation-cert-key.service" ];
+  };
+
+  users.users.cardano-node.extraGroups = [ "keys" ];
+
+  deployment.keys = {
+    "cardano-node-signing" = builtins.trace ("${name}: using " + (toString signingKey)) {
+        keyFile = signingKey;
+        user = "cardano-node";
+        group = "cardano-node";
+        destDir = "/var/lib/keys";
+    };
+    "cardano-node-delegation-cert" = builtins.trace ("${name}: using " + (toString delegationCertificate)) {
+        keyFile = delegationCertificate;
+        user = "cardano-node";
+        group = "cardano-node";
+        destDir = "/var/lib/keys";
+    };
+  };
 
 }
