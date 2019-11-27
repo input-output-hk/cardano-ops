b86378f0f676de66176c2bea74ad87c12bdb24b9
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2019-11-27 12:44:29 +0100

    Add orgs to nodes

--- topologies/shelley-dev.nix
+++ topologies/shelley-dev.nix
@@ -3,18 +3,21 @@
     {
       name = "a";
       nodeId = 0;
+      org = "IOHK";
       region = "eu-central-1";
       producers = ["b" "c"];
     }
     {
       name = "b";
       nodeId = 1;
+      org = "IOHK";
       region = "eu-central-1";
       producers = ["c" "a"];
     }
     {
       name = "c";
       nodeId = 2;
+      org = "IOHK";
       region = "eu-central-1";
       producers = ["a" "b"];
     }
