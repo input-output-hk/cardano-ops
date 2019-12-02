6798b5a0e1cd582599665616c894deb743156aa0
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2019-12-02 11:34:45 +0100

    mainnet: add correct dynamic-subsribe relay nodes.

--- topologies/mainnet.nix
+++ topologies/mainnet.nix
@@ -160,35 +160,384 @@
       ];
       org = "IOHK";
     }
+
     {
-      name = "u-a-1";
+      name = "p-b-1";
       region = "ap-northeast-1";
-      dynamicSubscribe = [
-        [ "r-a-1" "r-d-1" ]
-        [ "r-a-2" "r-c-1" "r-b-2" ]
-        [ "b-a-1" "b-b-1" ]
-      ];
       org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-d-1" "r-a-2"]
+        [ "r-b-2" "r-c-2" "r-c-1" "r-a-1" ]
+      ];
     }
     {
-      name = "u-b-1";
+      name = "p-b-2";
       region = "ap-northeast-1";
-      dynamicSubscribe = [
-        [ "r-c-1" "r-d-1" ]
-        [ "r-b-2" "r-a-2" "r-c-2" ]
-        [ "b-b-1" "b-c-1" ]
-      ];
       org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-d-1" "r-c-2"]
+        [ "r-b-2" "r-a-1" "r-a-2" "r-c-1" ]
+      ];
     }
     {
-      name = "u-c-1";
+      name = "p-b-3";
+      region = "ap-northeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-d-1" "r-c-2"]
+        [ "r-b-2" "r-c-1" "r-a-2" "r-a-1" ]
+      ];
+    }
+    {
+      name = "p-b-4";
+      region = "ap-northeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-a-2" "r-c-1"]
+        [ "r-b-2" "r-c-2" "r-a-1" "r-d-1" ]
+      ];
+    }
+    {
+      name = "p-b-5";
+      region = "ap-northeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-c-1" "r-a-1"]
+        [ "r-b-2" "r-d-1" "r-a-2" "r-c-2" ]
+      ];
+    }
+    {
+      name = "p-b-6";
+      region = "ap-northeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-c-2" "r-d-1"]
+        [ "r-b-2" "r-a-2" "r-a-1" "r-c-1" ]
+      ];
+    }
+    {
+      name = "p-b-7";
+      region = "ap-northeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-d-1" "r-c-2"]
+        [ "r-b-2" "r-c-1" "r-a-2" "r-a-1" ]
+      ];
+    }
+    {
+      name = "p-b-8";
+      region = "ap-northeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-d-1" "r-c-1"]
+        [ "r-b-2" "r-c-2" "r-a-2" "r-a-1" ]
+      ];
+    }
+    {
+      name = "p-b-9";
+      region = "ap-northeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-a-1" "r-c-1"]
+        [ "r-b-2" "r-c-2" "r-d-1" "r-a-2" ]
+      ];
+    }
+    {
+      name = "p-b-10";
+      region = "ap-northeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-c-1" "r-c-2"]
+        [ "r-b-2" "r-d-1" "r-a-1" "r-a-2" ]
+      ];
+    }
+    {
+      name = "p-b-11";
+      region = "ap-northeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-a-1" "r-c-1"]
+        [ "r-b-2" "r-a-2" "r-c-2" "r-d-1" ]
+      ];
+    }
+    {
+      name = "p-b-12";
+      region = "ap-northeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-c-1" "r-a-2"]
+        [ "r-b-2" "r-a-1" "r-c-2" "r-d-1" ]
+      ];
+    }
+    {
+      name = "p-b-13";
+      region = "ap-northeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-a-1" "r-c-1"]
+        [ "r-b-2" "r-d-1" "r-c-2" "r-a-2" ]
+      ];
+    }
+    {
+      name = "p-b-14";
+      region = "ap-northeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-a-1" "r-c-2"]
+        [ "r-b-2" "r-a-2" "r-d-1" "r-c-1" ]
+      ];
+    }
+    {
+      name = "p-b-15";
+      region = "ap-northeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-d-1" "r-c-1"]
+        [ "r-b-2" "r-a-2" "r-a-1" "r-c-2" ]
+      ];
+    }
+    {
+      name = "p-b-16";
+      region = "ap-northeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-c-1" "r-d-1"]
+        [ "r-b-2" "r-a-1" "r-c-2" "r-a-2" ]
+      ];
+    }
+    {
+      name = "p-b-17";
+      region = "ap-northeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-d-1" "r-c-1"]
+        [ "r-b-2" "r-a-2" "r-a-1" "r-d-1" ]
+      ];
+    }
+    {
+      name = "p-b-18";
+      region = "ap-northeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-c-2" "r-d-1"]
+        [ "r-b-2" "r-c-1" "r-a-1" "r-a-2" ]
+      ];
+    }
+    {
+      name = "p-b-19";
+      region = "ap-northeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-a-1" "r-c-1"]
+        [ "r-b-2" "r-a-2" "r-c-2" "r-d-1" ]
+      ];
+    }
+    {
+      name = "p-b-20";
+      region = "ap-northeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-a-1" "r-c-1"]
+        [ "r-b-2" "r-a-2" "r-d-1" "r-c-2" ]
+      ];
+    }
+    {
+      name = "p-b-21";
+      region = "ap-northeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-b-1" "r-d-1" "r-a-2"]
+        [ "r-b-2" "r-c-1" "r-a-1" "r-c-2" ]
+      ];
+    }
+    {
+      name = "p-c-1";
       region = "ap-southeast-1";
-      dynamicSubscribe = [
-        [ "r-c-1" "r-d-1" ]
-        [ "r-c-2" "r-a-2" "r-b-2" ]
-        [ "b-c-1" "b-a-1" ]
-      ];
       org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-a-2" "r-b-2"]
+        [ "r-c-2" "r-a-1" "r-b-1" "r-d-1" ]
+      ];
+    }
+    {
+      name = "p-c-2";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-b-2" "r-b-1"]
+        [ "r-c-2" "r-a-1" "r-d-1" "r-a-2" ]
+      ];
+    }
+    {
+      name = "p-c-3";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-d-1" "r-a-1"]
+        [ "r-c-2" "r-a-2" "r-b-1" "r-b-2" ]
+      ];
+    }
+    {
+      name = "p-c-4";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-b-2" "r-a-2"]
+        [ "r-c-2" "r-d-1" "r-a-1" "r-b-1" ]
+      ];
+    }
+    {
+      name = "p-c-5";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-b-2" "r-a-1"]
+        [ "r-c-2" "r-a-2" "r-d-1" "r-b-1" ]
+      ];
+    }
+    {
+      name = "p-c-6";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-d-1" "r-a-1"]
+        [ "r-c-2" "r-b-1" "r-b-2" "r-a-2" ]
+      ];
+    }
+    {
+      name = "p-c-7";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-a-2" "r-d-1"]
+        [ "r-c-2" "r-b-2" "r-a-1" "r-b-1" ]
+      ];
+    }
+    {
+      name = "p-c-8";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-a-1" "r-b-1"]
+        [ "r-c-2" "r-d-1" "r-b-2" "r-a-2" ]
+      ];
+    }
+    {
+      name = "p-c-9";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-a-2" "r-b-2"]
+        [ "r-c-2" "r-d-1" "r-a-1" "r-b-1" ]
+      ];
+    }
+    {
+      name = "p-c-10";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-a-1" "r-d-1"]
+        [ "r-c-2" "r-b-2" "r-b-1" "r-a-2" ]
+      ];
+    }
+    {
+      name = "p-c-11";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-d-1" "r-b-2"]
+        [ "r-c-2" "r-a-1" "r-b-1" "r-a-2" ]
+      ];
+    }
+    {
+      name = "p-c-12";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-a-1" "r-d-1"]
+        [ "r-c-2" "r-b-2" "r-a-2" "r-b-1" ]
+      ];
+    }
+    {
+      name = "p-c-13";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-a-2" "r-b-2"]
+        [ "r-c-2" "r-d-1" "r-b-1" "r-a-1" ]
+      ];
+    }
+    {
+      name = "p-c-14";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-b-1" "r-a-1"]
+        [ "r-c-2" "r-a-2" "r-d-1" "r-b-2" ]
+      ];
+    }
+    {
+      name = "p-c-15";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-a-2" "r-b-2"]
+        [ "r-c-2" "r-d-1" "r-b-1" "r-a-1" ]
+      ];
+    }
+    {
+      name = "p-c-16";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-b-1" "r-a-1"]
+        [ "r-c-2" "r-a-2" "r-d-1" "r-b-2" ]
+      ];
+    }
+    {
+      name = "p-c-17";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-d-1" "r-b-2"]
+        [ "r-c-2" "r-b-1" "r-a-2" "r-a-1" ]
+      ];
+    }
+    {
+      name = "p-c-18";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-b-2" "r-a-1"]
+        [ "r-c-2" "r-d-1" "r-a-2" "r-b-1" ]
+      ];
+    }
+    {
+      name = "p-c-19";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-a-2" "r-a-1"]
+        [ "r-c-2" "r-b-2" "r-b-1" "r-d-1" ]
+      ];
+    }
+    {
+      name = "p-c-20";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-b-1" "r-d-1"]
+        [ "r-c-2" "r-a-1" "r-b-2" "r-a-2" ]
+      ];
+    }
+    {
+      name = "p-c-21";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      dynamicSubscribe = [
+        [ "r-c-1" "r-b-2" "r-a-1"]
+        [ "r-c-2" "r-d-1" "r-a-2" "r-b-1" ]
+      ];
     }
   ];
 
