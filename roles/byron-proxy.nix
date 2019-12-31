16b297f7ee737ba4b1ade0609f506abbbd06edc0
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2019-12-31 12:34:44 +0100

    byron-proxy: specify shelley block producers

--- roles/byron-proxy.nix
+++ roles/byron-proxy.nix
@@ -1,5 +1,7 @@
 { name
 , config
+, nodes
+, resources
 , ...
 }:
 with import ../nix {};
@@ -7,6 +9,19 @@ let
   iohkNix = import sourcePaths.iohk-nix {};
   inherit (iohkNix) cardanoLib;
   legacyCardanoCfg = config.services.cardano-node-legacy;
+  nodePort = globals.cardanoNodePort;
+  hostName = name: "${name}.cardano";
+  staticRouteIp = getStaticRouteIp resources nodes;
+  cardanoNodes = lib.filterAttrs
+    (_: node: node.config.node.roles.isCardanoCore)
+    nodes;
+  coreCardanoHostList = lib.mapAttrsToList (nodeName: node: {
+    name = hostName nodeName;
+    ip = staticRouteIp nodeName;
+  }) coreCardanoNodes;
+  producersOpts = toString (map
+    (c: "--producer-addr [${c.name}]:${toString nodePort}")
+    coreCardanoHostList);
 in {
 
   imports = [
@@ -29,6 +44,7 @@ in {
     listen = "${legacyCardanoCfg.listenIp}:${toString globals.cardanoNodeLegacyPort}";
     address = "${legacyCardanoCfg.publicIp}:${toString globals.cardanoNodeLegacyPort}";
     topologyFile = legacyCardanoCfg.topologyYaml;
+    extraOptions = producersOpts;
   };
   systemd.services.byron-proxy.serviceConfig.MemoryMax = "3.5G";
 
@@ -36,4 +52,8 @@ in {
   networking.firewall.allowedTCPPorts = [ globals.cardanoNodePort ];
 
   services.cardano-node-legacy.nodeType = "relay";
+
+  networking.extraHosts = ''
+      ${lib.concatStringsSep "\n" (map (host: "${host.ip} ${host.name}") coreCardanoHostList)}
+  '';
 }
