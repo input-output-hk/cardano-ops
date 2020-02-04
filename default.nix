ce1ba996b6108ac1d71a345fcf1c1529cea7cdee
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2020-02-04 18:11:05 +0100

    Add key migration script

--- default.nix
+++ default.nix
@@ -6,6 +6,9 @@
 
   shell = let
     cardanoSL = import sourcePaths.cardano-sl {};
+    genesisFile = (import sourcePaths.iohk-nix {}).cardanoLib.environments.${globals.environmentName}.genesisFile or "please/set/globals.environmentName";
+
+
     mkDevGenesis = writeShellScriptBin "make-dev-genesis" (builtins.replaceStrings
       [ "\${RUNNER}"
         "SCRIPTDIR=$(dirname $0)"
@@ -17,9 +20,22 @@
         ("TARGETDIR=\"" + toString ./keys)
         ""
       ]
-     (builtins.readFile (sourcePaths.cardano-node + "/scripts/genesis.sh")));
+      (builtins.readFile (sourcePaths.cardano-node + "/scripts/genesis.sh")));
+    migrate-keys = writeShellScriptBin "migrate-keys" ''
+        i=0
+        for k in keys/*.sk; do
+          ((i++))
+          signing_key=keys/delegate-keys.00$i.key
+          echo "migrating $k to $signing_key"
+          cardano-cli migrate-delegate-key-from --byron-legacy --from $k --real-pbft --to $signing_key
+          pk=$(cardano-cli signing-key-public --real-pbft --secret $signing_key | fgrep 'public key (base64):' | cut -d: -f2 | xargs echo -n)
+          delegate_cert=keys/delegation-cert.00$i.json
+          echo "generating delegation certificate for $pk in $delegate_cert"
+          ${jq}/bin/jq ".heavyDelegation | .[] | select(.delegatePk == \"$pk\")" < ${genesisFile} > $delegate_cert
+        done
+     '';
   in  mkShell {
-    buildInputs = [ niv nixops nix cardano-cli telnet dnsutils mkDevGenesis nix-diff ] ++
+    buildInputs = [ niv nixops nix cardano-cli telnet dnsutils mkDevGenesis nix-diff migrate-keys ] ++
                   (with cardanoSL.nix-tools.exes; [ cardano-sl-auxx cardano-sl-tools ]);
     NIX_PATH = "nixpkgs=${path}";
     NIXOPS_DEPLOYMENT = "${globals.deploymentName}";
