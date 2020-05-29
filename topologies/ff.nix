91668a3a10f042472181936a29f5c7ddc3f9929f
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2020-05-29 14:21:46 +0200

    Add more relays

--- topologies/ff.nix
+++ topologies/ff.nix
@@ -7,7 +7,7 @@ let
       region = "eu-central-1";
       org = "IOHK";
       nodeId = 8;
-      producers = [ "c-a-1" "c-a-2" "e-b-1" "e-c-1" ];
+      producers = [ "c-a-1" "e-b-1" "e-c-1" "e-d-1" "e-a-2" ];
       services.cardano-node.profiling = "time";
     }
     {
@@ -15,14 +15,51 @@ let
       region = "ap-northeast-1";
       org = "IOHK";
       nodeId = 9;
-      producers = [ "c-b-1" "c-b-2" "e-c-1" "e-a-1" ];
+      producers = [ "c-b-1" "e-c-1" "e-a-1" "e-d-1" "e-b-2" ];
     }
     {
       name = "e-c-1";
       region = "ap-southeast-1";
       org = "IOHK";
       nodeId = 10;
-      producers = [ "c-c-1" "c-c-2" "e-a-1" "e-b-1"  ];
+      producers = [ "c-c-1" "e-a-1" "e-b-1" "e-d-1" "e-c-2" ];
+    }
+    {
+      name = "e-d-1";
+      region = "us-east-2";
+      org = "IOHK";
+      nodeId = 11;
+      producers = [ "c-a-1" "c-b-1" "c-c-1" "e-a-1" "e-b-1" "e-c-1" "e-d-2" ];
+    }
+
+    {
+      name = "e-a-2";
+      region = "eu-central-1";
+      org = "IOHK";
+      nodeId = 12;
+      producers = [ "c-a-2" "e-b-2" "e-c-2" "e-d-2" "e-a-1" ];
+      services.cardano-node.profiling = "time";
+    }
+    {
+      name = "e-b-2";
+      region = "ap-northeast-1";
+      org = "IOHK";
+      nodeId = 13;
+      producers = [ "c-b-2" "e-c-2" "e-a-2" "e-d-2" "e-b-1" ];
+    }
+    {
+      name = "e-c-2";
+      region = "ap-southeast-1";
+      org = "IOHK";
+      nodeId = 14;
+      producers = [ "c-c-2" "e-a-2" "e-b-2" "e-d-2" "e-c-1" ];
+    }
+    {
+      name = "e-d-2";
+      region = "us-east-2";
+      org = "IOHK";
+      nodeId = 15;
+      producers = [ "c-a-2" "c-b-2" "c-c-2" "e-a-2" "e-b-2" "e-c-2" "e-d-1" ];
     }
   ];
 
@@ -50,7 +87,7 @@ in {
     {
       name = "c-a-1";
       region = "eu-central-1";
-      producers = [ "c-b-1" "c-c-1" "c-a-2" "e-a-1" ];
+      producers = [ "c-b-1" "c-c-1" "c-a-2" "e-a-1" "e-d-1" ];
       org = "IOHK";
       nodeId = 1;
     }
@@ -72,21 +109,21 @@ in {
     {
       name = "c-a-2";
       region = "eu-central-1";
-      producers = [ "c-b-2" "c-c-2" "c-a-1" "e-a-1" ];
+      producers = [ "c-b-2" "c-c-2" "c-a-1" "e-a-2" "e-d-2" ];
       org = "IOHK";
       nodeId = 4;
     }
     {
       name = "c-b-2";
       region = "ap-northeast-1";
-      producers = [ "c-c-2" "c-a-2" "c-b-1" "e-b-1" ];
+      producers = [ "c-c-2" "c-a-2" "c-b-1" "e-b-2" ];
       org = "IOHK";
       nodeId = 5;
     }
     {
       name = "c-c-2";
       region = "ap-southeast-1";
-      producers = [ "c-a-2" "c-b-2" "c-c-1" "e-c-1" ];
+      producers = [ "c-a-2" "c-b-2" "c-c-1" "e-c-2" ];
       org = "IOHK";
       nodeId = 6;
     }
