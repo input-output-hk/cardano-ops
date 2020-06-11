7197c65c101abfed1bc73dfcf4a55215407c58a2
Arron <58341798+AWCrypt@users.noreply.github.com>
2020-06-11 11:01:04 +0100

    Addition of Planetstake.com relay nodes (#89)

--- topologies/ff-peers.nix
+++ topologies/ff-peers.nix
@@ -6,6 +6,20 @@
     operator = "disassembler";
     addr = "prophet.samleathers.com";
     port = 3001;
-    # valency = 1; (default)
+  }
+    {
+    operator = "planetstake";
+    addr = "stake-relay1.planetstake.com";
+    port = 3001;
+  }
+  {
+    operator = "planetstake";
+    addr = "stake-relay2.planetstake.com";
+    port = 3002;
+  }
+  {
+    operator = "planetstake";
+    addr = "stake-relay3.planetstake.com";
+    port = 3003;
   }
 ]
