983f3cdf9783e1660d1995f28d6f34e6010790ee
Venox20 <NickDeppe@icloud.com>
2020-06-12 21:13:37 +0200

    crown relays

--- topologies/ff-peers.nix
+++ topologies/ff-peers.nix
@@ -288,4 +288,25 @@
     port = 3001;
     valency = 2;
   }
+  {
+    operator = "CROWN";
+    node = "Relay1";
+    addr = "78.47.182.92";
+    port = 3001;
+    valency = 1;
+  }
+  {
+    operator = "CROWN";
+    node = "Relay2";
+    addr = "78.47.182.92";
+    port = 3002;
+    valency = 1;
+  }
+{
+    operator = "CROWN";
+    node = "Relay3";
+    addr = "94.130.99.102";
+    port = 3000;
+    valency = 1;
+  }
 ]
