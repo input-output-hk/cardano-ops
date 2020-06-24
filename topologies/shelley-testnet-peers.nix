3ca19bce005294d90c8a5e070030f905420784e2
Roberto C. Morano <rcmova@gmail.com>
2020-06-24 21:52:23 +0200

    added "RPST: La RepsistancE" shelley-testnet peers

---
+++ topologies/shelley-testnet-peers.nix
@@ -0,0 +1,11 @@
+# List of peers included in our relays topology config,
+# evenly split across all IOHK shelley-testnet relays.
+# Pull-requests against this file will be merged and deployed once a day.
+[
+  {
+    operator = "[RPST] La RepsistancE";
+    addr = "relay.stn.cardano.none.guru";
+    port = 31101;
+    valency = 2;
+  }
+]
