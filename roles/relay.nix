a81da2c933ecf51d5778e8d63cef623e76c1c7f8
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2020-07-16 11:46:31 +0200

    Use MaxConcurrencyDeadline = 4 on relays

--- roles/relay.nix
+++ roles/relay.nix
@@ -5,4 +5,9 @@ pkgs: {
     pkgs.cardano-ops.modules.base-service
   ];
 
+  services.cardano-node.nodeConfig = {
+    # The maximum number of used peers when fetching newly forged blocks.
+    MaxConcurrencyDeadline = 4;
+  };
+
 }
