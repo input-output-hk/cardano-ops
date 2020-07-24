006b4af4b0a8189882558c532f450f60d0da6a00
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2020-07-24 17:57:29 +0200

    Prepare testnet topology refactoring.

--- topologies/testnet.nix
+++ topologies/testnet.nix
@@ -1,22 +1,33 @@
-pkgs: with pkgs; with lib;
-{
-  "${globals.faucetHostname}" = {
-    services.cardano-faucet = {
-      anonymousAccess = true;
-      faucetLogLevel = "DEBUG";
-      secondsBetweenRequestsAnonymous = 86400;
-      secondsBetweenRequestsApiKeyAuth = 86400;
-      lovelacesToGiveAnonymous = 1000000000;
-      lovelacesToGiveApiKeyAuth = 1000000000000;
-      faucetFrontendUrl = "https://testnets.cardano.org/en/byron/tools/faucet/";
+pkgs: with pkgs; with lib; with topology-lib;
+let
+
+  withAutoRestart = def: lib.recursiveUpdate {
+    systemd.services.cardano-node.serviceConfig = {
+      RuntimeMaxSec = 6 *
+        60 * 60 + 60 * (5 * (def.nodeId or 0));
+    };
+  } def;
+
+  regions = {
+    a = { name = "eu-central-1";   # Europe (Frankfurt);
+      minRelays = 2;
+    };
+    b = { name = "us-east-2";      # US East (Ohio)
+      minRelays = 2;
+    };
+    c = { name = "ap-southeast-1"; # Asia Pacific (Singapore)
+      minRelays = 2;
+    };
+    d = { name = "eu-west-2";      # Europe (London)
+      minRelays = 2;
+    };
+    e = { name = "us-west-1";      # US West (N. California)
+      minRelays = 2;
+    };
+    f = { name = "ap-northeast-1"; # Asia Pacific (Tokyo)
+      minRelays = 2;
     };
   };
-
-  legacyCoreNodes = [];
-
-  legacyRelayNodes = [];
-
-  byronProxies = [];
 
   coreNodes = [
     {
@@ -27,6 +38,7 @@ pkgs: with pkgs; with lib;
         "c-b-1" "c-c-1" "c-d-1"
         "e-a-1" "e-a-2"
         "e-b-1"
+        "rel-a-1" "rel-a-2"
       ];
       org = "IOHK";
       nodeId = 1;
@@ -39,6 +51,7 @@ pkgs: with pkgs; with lib;
         "c-b-2" "c-c-2"
         "e-a-1" "e-a-2"
         "e-c-1" "e-d-1"
+        "rel-a-1" "rel-a-2"
       ];
       org = "IOHK";
       nodeId = 2;
@@ -51,6 +64,7 @@ pkgs: with pkgs; with lib;
         "c-a-1" "c-c-1" "c-d-1"
         "e-b-1" "e-b-2"
         "e-a-1"
+        "rel-f-1" "rel-f-2"
       ];
       org = "IOHK";
       nodeId = 3;
@@ -63,6 +77,7 @@ pkgs: with pkgs; with lib;
         "c-a-2" "c-c-2"
         "e-b-1" "e-b-2"
         "e-c-2" "e-d-2"
+        "rel-f-1" "rel-f-2"
       ];
       org = "IOHK";
       nodeId = 4;
@@ -75,6 +90,7 @@ pkgs: with pkgs; with lib;
         "c-a-1" "c-b-1" "c-d-1"
         "e-c-1" "e-c-2"
         "e-a-1"
+        "rel-c-1" "rel-c-2"
       ];
       org = "IOHK";
       nodeId = 5;
@@ -87,6 +103,7 @@ pkgs: with pkgs; with lib;
         "c-a-2" "c-b-2"
         "e-c-1" "e-c-2"
         "e-b-2" "e-d-2"
+        "rel-c-1" "rel-c-2"
       ];
       org = "IOHK";
       nodeId = 6;
@@ -98,13 +115,89 @@ pkgs: with pkgs; with lib;
         "c-a-1" "c-b-1" "c-c-1"
         "e-d-1" "e-d-2"
         "e-a-1" "e-b-1" "e-c-1"
+        "rel-b-1" "rel-b-2"
       ];
       org = "IOHK";
       nodeId = 7;
     }
    ];
 
-  relayNodes = [
+   nextCoreNodes = [
+    # OBFT centralized nodes
+    {
+      name = "bft-a-1";
+      region = regions.a.name;
+      producers = map (c: c.name) nextCoreNodes ++ [ "rel-a-1" "rel-a-2" ];
+      org = "IOHK";
+      nodeId = 1;
+    }
+    {
+      name = "bft-b-1";
+      region = regions.b.name;
+      producers = map (c: c.name) nextCoreNodes ++ [ "rel-b-1" "rel-b-2" ];
+      org = "IOHK";
+      nodeId = 2;
+    }
+    {
+      name = "bft-c-1";
+      region = regions.c.name;
+      producers = map (c: c.name) nextCoreNodes ++ [ "rel-c-1" "rel-c-2" ];
+      org = "IOHK";
+      nodeId = 3;
+    }
+    {
+      name = "bft-d-1";
+      region = regions.b.name;
+      producers = map (c: c.name) nextCoreNodes ++ [ "rel-d-1" "rel-d-2" ];
+      org = "IOHK";
+      nodeId = 4;
+    }
+    {
+      name = "bft-e-1";
+      region = regions.c.name;
+      producers = map (c: c.name) nextCoreNodes ++ [ "rel-e-1" "rel-e-2" ];
+      org = "IOHK";
+      nodeId = 5;
+    }
+    {
+      name = "bft-f-1";
+      region = regions.f.name;
+      producers = map (c: c.name) nextCoreNodes ++ [ "rel-f-1" "rel-f-2" ];
+      org = "IOHK";
+      nodeId = 6;
+    }
+    {
+      name = "bft-a-2";
+      region = regions.a.name;
+      producers = map (c: c.name) nextCoreNodes ++ [ "rel-a-1" "rel-a-2" ];
+      org = "IOHK";
+      nodeId = 7;
+    }
+    # stake pools
+    {
+      name = "stk-a-1";
+      region = regions.a.name;
+      producers = map (c: c.name) nextCoreNodes ++ [ "rel-a-1" "rel-a-2" ];
+      org = "IOHK";
+      nodeId = 8;
+    }
+    {
+      name = "stk-b-1";
+      region = regions.b.name;
+      producers = map (c: c.name) nextCoreNodes ++ [ "rel-b-1" "rel-b-2" ];
+      org = "IOHK";
+      nodeId = 9;
+    }
+    {
+      name = "stk-c-1";
+      region = regions.c.name;
+      producers = map (c: c.name) nextCoreNodes ++ [ "rel-c-1" "rel-c-2" ];
+      org = "IOHK";
+      nodeId = 10;
+    }
+  ];
+
+  oldRelayNodes = [
     {
       name = "e-a-1";
       region = "eu-central-1";
@@ -162,4 +255,30 @@ pkgs: with pkgs; with lib;
       producers = ["c-d-1" "e-d-1" "e-a-2" "e-b-2" "e-c-2"];
     }
   ];
+
+in {
+  inherit coreNodes;
+
+  relayNodes = map withAutoRestart (mkRelayTopology {
+    inherit regions;
+    coreNodes = coreNodes ++ nextCoreNodes;
+  }) ++ nextCoreNodes ++ oldRelayNodes;
+
+  "${globals.faucetHostname}" = {
+    services.cardano-faucet = {
+      anonymousAccess = true;
+      faucetLogLevel = "DEBUG";
+      secondsBetweenRequestsAnonymous = 86400;
+      secondsBetweenRequestsApiKeyAuth = 86400;
+      lovelacesToGiveAnonymous = 1000000000;
+      lovelacesToGiveApiKeyAuth = 1000000000000;
+      faucetFrontendUrl = "https://testnets.cardano.org/en/byron/tools/faucet/";
+    };
+  };
+
+  legacyCoreNodes = [];
+
+  legacyRelayNodes = [];
+
+  byronProxies = [];
 }
