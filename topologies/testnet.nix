54ac4fce3c5aa033122d716132c02bdde231bc25
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2020-07-24 19:32:25 +0200

    Prune old node from testnet topologies

--- topologies/testnet.nix
+++ topologies/testnet.nix
@@ -30,146 +30,53 @@ let
   };
 
   coreNodes = [
-    {
-      name = "c-a-1";
-      region = "eu-central-1";
-      producers = [
-        "c-a-2"
-        "c-b-1" "c-c-1" "c-d-1"
-        "e-a-1" "e-a-2"
-        "e-b-1"
-        "rel-a-1" "rel-a-2"
-      ];
-      org = "IOHK";
-      nodeId = 1;
-    }
-    {
-      name = "c-a-2";
-      region = "eu-central-1";
-      producers = [
-        "c-a-1"
-        "c-b-2" "c-c-2"
-        "e-a-1" "e-a-2"
-        "e-c-1" "e-d-1"
-        "rel-a-1" "rel-a-2"
-      ];
-      org = "IOHK";
-      nodeId = 2;
-    }
-    {
-      name = "c-b-1";
-      region = "ap-northeast-1";
-      producers = [
-        "c-b-2"
-        "c-a-1" "c-c-1" "c-d-1"
-        "e-b-1" "e-b-2"
-        "e-a-1"
-        "rel-f-1" "rel-f-2"
-      ];
-      org = "IOHK";
-      nodeId = 3;
-    }
-    {
-      name = "c-b-2";
-      region = "ap-northeast-1";
-      producers = [
-        "c-b-1"
-        "c-a-2" "c-c-2"
-        "e-b-1" "e-b-2"
-        "e-c-2" "e-d-2"
-        "rel-f-1" "rel-f-2"
-      ];
-      org = "IOHK";
-      nodeId = 4;
-    }
-    {
-      name = "c-c-1";
-      region = "ap-southeast-1";
-      producers = [
-        "c-c-2"
-        "c-a-1" "c-b-1" "c-d-1"
-        "e-c-1" "e-c-2"
-        "e-a-1"
-        "rel-c-1" "rel-c-2"
-      ];
-      org = "IOHK";
-      nodeId = 5;
-    }
-    {
-      name = "c-c-2";
-      region = "ap-southeast-1";
-      producers = [
-        "c-c-1"
-        "c-a-2" "c-b-2"
-        "e-c-1" "e-c-2"
-        "e-b-2" "e-d-2"
-        "rel-c-1" "rel-c-2"
-      ];
-      org = "IOHK";
-      nodeId = 6;
-    }
-    {
-      name = "c-d-1";
-      region = "us-east-2";
-      producers = [
-        "c-a-1" "c-b-1" "c-c-1"
-        "e-d-1" "e-d-2"
-        "e-a-1" "e-b-1" "e-c-1"
-        "rel-b-1" "rel-b-2"
-      ];
-      org = "IOHK";
-      nodeId = 7;
-    }
-   ];
-
-   nextCoreNodes = [
     # OBFT centralized nodes
     {
       name = "bft-a-1";
       region = regions.a.name;
-      producers = map (c: c.name) nextCoreNodes ++ [ "rel-a-1" "rel-a-2" ];
+      producers = map (c: c.name) coreNodes ++ [ "rel-a-1" "rel-a-2" ];
       org = "IOHK";
       nodeId = 1;
     }
     {
       name = "bft-b-1";
       region = regions.b.name;
-      producers = map (c: c.name) nextCoreNodes ++ [ "rel-b-1" "rel-b-2" ];
+      producers = map (c: c.name) coreNodes ++ [ "rel-b-1" "rel-b-2" ];
       org = "IOHK";
       nodeId = 2;
     }
     {
       name = "bft-c-1";
       region = regions.c.name;
-      producers = map (c: c.name) nextCoreNodes ++ [ "rel-c-1" "rel-c-2" ];
+      producers = map (c: c.name) coreNodes ++ [ "rel-c-1" "rel-c-2" ];
       org = "IOHK";
       nodeId = 3;
     }
     {
       name = "bft-d-1";
       region = regions.b.name;
-      producers = map (c: c.name) nextCoreNodes ++ [ "rel-d-1" "rel-d-2" ];
+      producers = map (c: c.name) coreNodes ++ [ "rel-d-1" "rel-d-2" ];
       org = "IOHK";
       nodeId = 4;
     }
     {
       name = "bft-e-1";
       region = regions.c.name;
-      producers = map (c: c.name) nextCoreNodes ++ [ "rel-e-1" "rel-e-2" ];
+      producers = map (c: c.name) coreNodes ++ [ "rel-e-1" "rel-e-2" ];
       org = "IOHK";
       nodeId = 5;
     }
     {
       name = "bft-f-1";
       region = regions.f.name;
-      producers = map (c: c.name) nextCoreNodes ++ [ "rel-f-1" "rel-f-2" ];
+      producers = map (c: c.name) coreNodes ++ [ "rel-f-1" "rel-f-2" ];
       org = "IOHK";
       nodeId = 6;
     }
     {
       name = "bft-a-2";
       region = regions.a.name;
-      producers = map (c: c.name) nextCoreNodes ++ [ "rel-a-1" "rel-a-2" ];
+      producers = map (c: c.name) coreNodes ++ [ "rel-a-1" "rel-a-2" ];
       org = "IOHK";
       nodeId = 7;
     }
@@ -177,82 +84,23 @@ let
     {
       name = "stk-a-1";
       region = regions.a.name;
-      producers = map (c: c.name) nextCoreNodes ++ [ "rel-a-1" "rel-a-2" ];
+      producers = map (c: c.name) coreNodes ++ [ "rel-a-1" "rel-a-2" ];
       org = "IOHK";
       nodeId = 8;
     }
     {
       name = "stk-b-1";
       region = regions.b.name;
-      producers = map (c: c.name) nextCoreNodes ++ [ "rel-b-1" "rel-b-2" ];
+      producers = map (c: c.name) coreNodes ++ [ "rel-b-1" "rel-b-2" ];
       org = "IOHK";
       nodeId = 9;
     }
     {
       name = "stk-c-1";
       region = regions.c.name;
-      producers = map (c: c.name) nextCoreNodes ++ [ "rel-c-1" "rel-c-2" ];
+      producers = map (c: c.name) coreNodes ++ [ "rel-c-1" "rel-c-2" ];
       org = "IOHK";
       nodeId = 10;
-    }
-  ];
-
-  oldRelayNodes = [
-    {
-      name = "e-a-1";
-      region = "eu-central-1";
-      org = "IOHK";
-      nodeId = 8;
-      producers = ["c-a-2" "e-a-2" "e-b-1" "e-c-1" "e-d-1"];
-    }
-    {
-      name = "e-b-1";
-      region = "ap-northeast-1";
-      org = "IOHK";
-      nodeId = 9;
-      producers = ["c-b-2" "e-b-2" "e-a-1" "e-d-1" "e-c-1"];
-    }
-    {
-      name = "e-c-1";
-      region = "ap-southeast-1";
-      org = "IOHK";
-      nodeId = 10;
-      producers = ["c-c-2" "e-c-2" "e-d-1" "e-a-1" "e-b-1"];
-    }
-    {
-      name = "e-d-1";
-      region = "us-east-2";
-      org = "IOHK";
-      nodeId = 11;
-      producers = ["c-d-1" "e-d-2" "e-c-1" "e-a-1" "e-b-1"];
-    }
-    {
-      name = "e-a-2";
-      region = "eu-central-1";
-      org = "IOHK";
-      nodeId = 12;
-      producers = ["c-a-1" "e-a-1" "e-b-2" "e-c-2" "e-d-2"];
-    }
-    {
-      name = "e-b-2";
-      region = "ap-northeast-1";
-      org = "IOHK";
-      nodeId = 13;
-      producers = ["c-b-1" "e-b-1" "e-a-2" "e-c-2" "e-d-2"];
-    }
-    {
-      name = "e-c-2";
-      region = "ap-southeast-1";
-      org = "IOHK";
-      nodeId = 14;
-      producers = ["c-c-1" "e-c-1" "e-a-2" "e-b-2" "e-d-2"];
-    }
-    {
-      name = "e-d-2";
-      region = "us-east-2";
-      org = "IOHK";
-      nodeId = 15;
-      producers = ["c-d-1" "e-d-1" "e-a-2" "e-b-2" "e-c-2"];
     }
   ];
 
@@ -261,8 +109,8 @@ in {
 
   relayNodes = map withAutoRestart (mkRelayTopology {
     inherit regions;
-    coreNodes = coreNodes ++ nextCoreNodes;
-  }) ++ nextCoreNodes ++ oldRelayNodes;
+    coreNodes = coreNodes;
+  });
 
   "${globals.faucetHostname}" = {
     services.cardano-faucet = {
