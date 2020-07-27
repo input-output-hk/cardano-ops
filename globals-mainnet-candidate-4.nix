56306862aeb1b9511dec2d86863b874d19c4df10
John Lotoski <john.lotoski@iohk.io>
2020-07-27 15:59:37 -0400

    Adds MC4 global and topology

---
+++ globals-mainnet-candidate-4.nix
@@ -0,0 +1,30 @@
+pkgs: {
+
+  deploymentName = "mainnet-candidate-4";
+
+  environmentName = "mainnet_candidate_4";
+
+  topology = import ./topologies/mainnet-candidate-4.nix pkgs;
+
+  withExplorer = true;
+  withLegacyExplorer = false;
+  withHighLoadRelays = true;
+  withSmash = true;
+
+  withFaucet = true;
+  faucetHostname = "faucet";
+
+  ec2 = {
+    credentials = {
+      accessKeyIds = {
+        IOHK = "default";
+        dns = "dev";
+      };
+    };
+  };
+
+  alertChainDensityLow = "60";
+  alertMemPoolHigh = "190";
+  alertTcpHigh = "150";
+  alertTcpCrit = "180";
+}
