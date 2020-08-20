617ce053ccfaee1308b3de0ad32e88c6ddf77088
John Lotoski <john.lotoski@iohk.io>
2020-08-20 11:48:27 -0400

    Updates shelley-qa topo for 3rd party producers

--- topologies/shelley-qa.nix
+++ topologies/shelley-qa.nix
@@ -1,6 +1,29 @@
-{
+pkgs: with pkgs; with lib; with topology-lib;
+let
+
+  withAutoRestart = def: lib.recursiveUpdate {
+    systemd.services.cardano-node.serviceConfig = {
+      RuntimeMaxSec = 6 *
+        60 * 60 + 60 * (5 * (def.nodeId or 0));
+    };
+  } def;
+
+in {
   monitoring = {
     services.monitoring-services.publicGrafana = false;
+  };
+
+
+  "${globals.faucetHostname}" = {
+    services.cardano-faucet = {
+      anonymousAccess = false;
+      faucetLogLevel = "DEBUG";
+      secondsBetweenRequestsAnonymous = 86400;
+      secondsBetweenRequestsApiKeyAuth = 86400;
+      lovelacesToGiveAnonymous = 1000000000;
+      lovelacesToGiveApiKeyAuth = 10000000000;
+      useByronWallet = false;
+    };
   };
 
   coreNodes = [
@@ -48,13 +71,6 @@
       org = "IOHK";
       nodeId = 6;
     }
-    {
-      name = "stk-d-1-IOHK4";
-      region = "us-east-1";
-      producers = [ "stk-a-1-IOHK1" "stk-b-1-IOHK2" "stk-c-1-IOHK3" "rel-d-1" ];
-      org = "IOHK";
-      nodeId = 7;
-    }
   ];
 
   relayNodes = [
@@ -64,28 +80,33 @@
       region = "eu-central-1";
       org = "IOHK";
       nodeId = 101;
-      producers = [ "bft-a-1" "stk-a-1-IOHK1" "rel-b-1" "rel-c-1" "rel-d-1" ];
+      producers = [ "bft-a-1" "stk-a-1-IOHK1" "rel-b-1" "rel-c-1" "rel-d-1" ] ++ thirdPartyRelays;
     }
     {
       name = "rel-b-1";
       region = "ap-northeast-1";
       org = "IOHK";
       nodeId = 102;
-      producers = [ "bft-b-1" "stk-b-1-IOHK2" "rel-c-1" "rel-a-1" "rel-d-1" ];
+      producers = [ "bft-b-1" "stk-b-1-IOHK2" "rel-c-1" "rel-a-1" "rel-d-1" ] ++ thirdPartyRelays;
     }
     {
       name = "rel-c-1";
       region = "ap-southeast-1";
       org = "IOHK";
       nodeId = 103;
-      producers = [ "bft-c-1" "stk-c-1-IOHK3" "rel-a-1" "rel-b-1" "rel-d-1" ];
+      producers = [ "bft-c-1" "stk-c-1-IOHK3" "rel-a-1" "rel-b-1" "rel-d-1" ] ++ thirdPartyRelays;
     }
     {
       name = "rel-d-1";
       region = "us-east-1";
       org = "IOHK";
       nodeId = 104;
-      producers = [ "stk-d-1-IOHK4" "rel-a-1" "rel-b-1"  ];
+      producers = [ "stk-d-1-IOHK4" "rel-a-1" "rel-b-1"  ] ++ thirdPartyRelays;
     }
   ];
+  explorer = {
+    services.nginx.virtualHosts."${globals.explorerHostName}.${globals.domain}".locations."/p" = lib.mkIf (__pathExists ../static/pool-metadata) {
+      root = ../static/pool-metadata;
+    };
+  };
 }
