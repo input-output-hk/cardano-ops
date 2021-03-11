0fb48b164a2e96ede4f4fa55c756c70408e279eb
Kosyrev Serge <serge.kosyrev@iohk.io>
2021-03-11 19:51:54 +0300

    CAD-2741 bench:  enable local mux trace

--- roles/tx-generator.nix
+++ roles/tx-generator.nix
@@ -137,6 +137,7 @@ in {
       TraceLocalTxSubmissionServer      = true;
       TraceMempool                      = true;
       TraceMux                          = true;
+      TraceLocalMux                     = true;
       TraceTxInbound                    = true;
       TraceTxOutbound                   = true;
       TraceTxSubmissionProtocol         = true;
