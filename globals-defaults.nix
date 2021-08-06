6af0ef9311a66a4d523acef243857dc7886c5179
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2021-08-06 16:03:56 +0200

    switch explorer-a to db.sync 10.

--- globals-defaults.nix
+++ globals-defaults.nix
@@ -30,15 +30,10 @@ in {
   explorerForceSSL = true;
   explorerAliases = [];
   explorerBackends = {
-    a = globals.explorer9;
+    a = globals.explorer10;
     b = globals.explorer10;
   };
-  explorerActiveBackends = [ "a" ];
-  explorer9 = {
-    cardano-db-sync = sourcePaths.cardano-db-sync-9;
-    cardano-graphql = sourcePaths.cardano-graphql-4;
-    cardano-explorer-app = sourcePaths."cardano-explorer-app-1.5";
-  };
+  explorerActiveBackends = [ "a" "b" ];
   explorer10 = {
     cardano-db-sync = sourcePaths.cardano-db-sync-10;
     cardano-graphql = sourcePaths.cardano-graphql-5;
