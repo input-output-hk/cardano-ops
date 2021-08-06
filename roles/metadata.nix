1808833f6608b20aeb180749dbe02f46690c985e
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2021-08-06 23:56:26 +0200

    metadata: reduce varnish malloc limit a bit to make room for memory leak

--- roles/metadata.nix
+++ roles/metadata.nix
@@ -116,7 +116,7 @@ in {
   services.varnish = {
     enable = true;
     extraModules = [ pkgs.varnish-modules ];
-    extraCommandLine = "-s malloc,${toString (config.node.memory * 1024 / 2)}M";
+    extraCommandLine = "-s malloc,${toString (config.node.memory * 1024 / 3)}M";
     config = ''
       vcl 4.1;
 
