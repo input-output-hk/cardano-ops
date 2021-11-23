70711a44369f97727e8455aae2201f591b8e46a4
Kosyrev Serge <serge.kosyrev@iohk.io>
2021-11-24 02:00:29 +0300

    bench:  node config factoring and maincanary deployment

---
+++ globals-bench-common.nix
@@ -0,0 +1,86 @@
+{ pkgs, benchmarkingProfile }:
+
+with pkgs.lib;
+rec {
+  mkNodeOverlay =
+    machineOverlay: nodeConfigOverlay:
+    recursiveUpdate
+    {
+      documentation = {
+        man.enable = false;
+        doc.enable = false;
+      };
+      networking.firewall.allowPing = mkForce true;
+      services.cardano-node = {
+        eventlog = mkForce true;
+        extraNodeConfig = mkExtraNodeConfig nodeConfigOverlay;
+        package = mkForce pkgs.cardano-node-eventlogged;
+        rtsArgs =
+          mkForce ([ "-N2" "-A16m" "-qg" "-qb" "-scardano-node.gcstats" ]
+                   ++
+                   (benchmarkingProfile.node.rts_flags_override or []));
+      };
+    } machineOverlay;
+
+  mkExtraNodeConfig =
+    { TraceBlockFetchProtocol ? false
+    , ... }:
+    recursiveUpdate
+      (removeAttrs pkgs.globals.environmentConfig.nodeConfig
+        ["ByronGenesisHash"
+         "ShelleyGenesisHash"
+         "AlonzoGenesisHash"])
+      (recursiveUpdate
+        (benchmarkingLogConfig "node")
+        ({
+           TracingVerbosity = "NormalVerbosity";
+           minSeverity = "Debug";
+           TurnOnLogMetrics = true;
+
+           TestEnableDevelopmentHardForkEras = true;
+           TestEnableDevelopmentNetworkProtocols = true;
+
+           inherit TraceBlockFetchProtocol;
+
+           TraceMempool               = true;
+           TraceTxInbound             = true;
+           TraceBlockFetchClient      = true;
+           TraceBlockFetchServer      = true;
+           TraceChainSyncHeaderServer = true;
+           TraceChainSyncClient       = true;
+        } //
+        (benchmarkingProfile.node.extra_config or {})));
+
+  benchmarkingLogConfig = name: {
+    defaultScribes = [
+      [ "StdoutSK" "stdout" ]
+      [ "FileSK"   "logs/${name}.json" ]
+    ];
+    setupScribes = [
+      {
+        scKind     = "StdoutSK";
+        scName     = "stdout";
+        scFormat   = "ScJson"; }
+      {
+        scKind     = "FileSK";
+        scName     = "logs/${name}.json";
+        scFormat   = "ScJson";
+        scRotation = {
+          ## 1. Twice-daily, so not too large, but also most benchmarks
+          ##    would be covered by that
+          rpMaxAgeHours   = 12;
+          ## 2. Ten per epoch, for two last epochs
+          rpKeepFilesNum  = 20;
+          ## 3. 10GB/file to prevent file-size cutoff from happening,
+          ##    and so most benchmarks will have just 1 file
+          rpLogLimitBytes = 10*1000*1000*1000;
+        }; }
+    ];
+    options = {
+      mapBackends = {
+        "cardano.node.resources" = [ "KatipBK" ];
+        "cardano.node.metrics"   = [ "EKGViewBK" ];
+      };
+    };
+  };
+}
---
+++ globals-maincanary.nix
@@ -0,0 +1,79 @@
+pkgs:
+
+with pkgs.lib;
+let
+  ## Stub, to enable use of globals-bench-common.nix
+  benchmarkingProfile = {
+  };
+
+  inherit (import ./globals-bench-common.nix { inherit pkgs benchmarkingProfile; })
+    mkNodeOverlay;
+
+  mkRegionRelayConnection =
+    region: {
+      addr = "${region}.relays-new.cardano-mainnet.iohk.io";
+      port = 3001;
+      valency = 3;
+    };
+in
+{
+
+  networkName = "Benchmarking-configured mainnet canary";
+  deploymentName = "maincanary";
+  environmentName = "mainnet";
+
+  relaysNew = "relays.${pkgs.globals.domain}";
+  nbInstancesPerRelay = 1;
+
+  withCardanoDBExtended = false;
+  withExplorer = false;
+  withMonitoring = false;
+
+  topology =
+    { coreNodes  = [];
+      relayNodes = [
+        ({
+          name = "maincanary";
+          nodeId = 0;
+          region = "eu-central-1";
+          org = "IOHK";
+          producers =
+            map mkRegionRelayConnection
+              [ "asia-pacific"
+                "europe"
+                "north-america"
+              ];
+
+          ###
+          documentation = {
+            man.enable = false;
+            doc.enable = false;
+          };
+          networking.firewall.allowPing = mkForce true;
+          services.cardano-node = {
+            package = mkForce pkgs.cardano-node-eventlogged;
+            extraNodeConfig = setupNodeConfig { TraceBlockFetchProtocol = true; };
+          };
+          systemd.services.dump-registered-relays-topology.enable = mkForce false;
+        }
+        // ## Mainnet relay with minimal benchmarking extras.
+        mkNodeOverlay {} {})
+      ];
+    };
+
+  ec2 = with pkgs.iohk-ops-lib.physical.aws;
+    {
+      instances = {
+        core-node = c5-2xlarge;
+        relay-node = c5-2xlarge;
+      };
+      credentials = {
+        accessKeyIds = {
+          IOHK = "dev-deployer";
+          dns = "dev-deployer";
+        };
+      };
+    };
+
+  nodeDbDiskAllocationSize = 200;
+}
