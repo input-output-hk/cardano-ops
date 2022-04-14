8a494bb455fb03aba2f53f37395273787f8a172e
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2022-04-14 09:19:06 +0200

    Fix missing space in script

--- scripts/resize-core-nodes-ebs-disks.sh
+++ scripts/resize-core-nodes-ebs-disks.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 cd "$(dirname "$0")/.."
 
-CORE_NODES=$(nix eval --impure --raw --expr'(toString (map (r: r.name) (import ./nix {}).globals.topology.coreNodes))')
+CORE_NODES=$(nix eval --impure --raw --expr '(toString (map (r: r.name) (import ./nix {}).globals.topology.coreNodes))')
 
 TARGET_SIZE=$(nix eval --impure --expr '(with (import ./nix {}).globals; systemDiskAllocationSize + nodeDbDiskAllocationSize)')
 
