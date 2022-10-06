02a16900149c35280b2bf5afa8ad931714b0d220
Kosyrev Serge <serge.kosyrev@iohk.io>
2022-10-06 09:34:04 +0800

    bench:  localise the bench-3 topo to eu-central-1

--- topologies/bench-dense-3.nix
+++ topologies/bench-dense-3.nix
@@ -11,7 +11,7 @@
       name = "node-1";
       nodeId = 1;
       org = "IOHK";
-      region = "ap-southeast-2";
+      region = "eu-central-1";
       producers = ["node-2" "node-0"];
       pools = 1;
     }
@@ -19,7 +19,7 @@
       name = "node-2";
       nodeId = 2;
       org = "IOHK";
-      region = "us-east-1";
+      region = "eu-central-1";
       producers = ["node-0" "node-1"];
       pools = 10;
     }
