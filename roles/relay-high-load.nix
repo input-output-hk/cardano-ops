96e69bde866c6ed2242ae950951f6199810fd5ee
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2023-02-20 19:33:17 +0100

    relays: set AcceptedConnectionsLimit according to alert setup

--- roles/relay-high-load.nix
+++ roles/relay-high-load.nix
@@ -1,6 +1,8 @@
 pkgs:
 with pkgs; with lib;
-{name, ...}: {
+{name, config, ...}: let
+ cfg = config.services.cardano-node;
+in {
 
   imports = [
     cardano-ops.roles.relay
@@ -8,4 +10,15 @@ with pkgs; with lib;
 
   # Add host and container auto metrics and alarming
   services.custom-metrics.enableNetdata = true;
+
+  services.cardano-node.extraNodeConfig = {
+    AcceptedConnectionsLimit = {
+      # Ensure limits are above our alerts threshold:
+      hardLimit = topology-lib.roundToInt
+        (globals.alertTcpCrit / cfg.instances * 1.05);
+      softLimit = topology-lib.roundToInt
+        (globals.alertTcpHigh / cfg.instances * 1.05);
+      delay = 5;
+    };
+  };
 }
