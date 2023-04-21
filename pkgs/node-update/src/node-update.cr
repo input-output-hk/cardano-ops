300bd53803078a555e6f63dfaa2356fe790ba4d7
John Lotoski <john.lotoski@iohk.io>
2023-04-21 16:42:08 -0500

    fix: correct cmd output parsing

--- pkgs/node-update/src/node-update.cr
+++ pkgs/node-update/src/node-update.cr
@@ -50,13 +50,13 @@ class RelayUpdate
 
     if (@emailOpt != "")
       if runCmdSecret("nix-instantiate --eval -E --json '(import #{PATH_MOD}/static/ses.nix).sesSmtp.username'").success?
-        @sesUsername = IO_CMD_OUT.to_s.strip('"')
+        @sesUsername = IO_CMD_OUT.to_s.strip('\n').strip('"')
       else
         abort("Unable to process the ses username.")
       end
 
       if runCmdSecret("nix-instantiate --eval -E --json '(import #{PATH_MOD}/static/ses.nix).sesSmtp.secret'").success?
-        @sesSecret = IO_CMD_OUT.to_s.strip('"')
+        @sesSecret = IO_CMD_OUT.to_s.strip('\n').strip('"')
       else
         abort("Unable to process the ses secret.")
       end
@@ -66,13 +66,13 @@ class RelayUpdate
     end
 
     if runCmdVerbose("nix-instantiate --eval -E --json '(import #{PATH_MOD}/nix {}).globals.environmentName'").success?
-      @cluster = IO_CMD_OUT.to_s.strip('"')
+      @cluster = IO_CMD_OUT.to_s.strip('\n').strip('"')
     else
       updateAbort("Unable to process the environment name from the globals file.")
     end
 
     if runCmdVerbose("nix-instantiate --eval -E --json '(import #{PATH_MOD}/nix {}).globals.deploymentName'").success?
-      @deployment = IO_CMD_OUT.to_s.strip('"')
+      @deployment = IO_CMD_OUT.to_s.strip('\n').strip('"')
     else
       updateAbort("Unable to process the deployment name from the globals file.")
     end
