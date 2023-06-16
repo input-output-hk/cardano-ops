6d5ba42c42d81b6c1b9a5548a3a5cf58eb3ad36a
John Lotoski <john.lotoski@iohk.io>
2023-06-16 18:22:37 -0500

    imp: enable non-systemd socket multi instance cfg

--- topologies/testp2p.nix
+++ topologies/testp2p.nix
@@ -53,10 +53,43 @@ let
 
   relayNodes = map (composeAll [
     (forNodes {
+      networking.localCommands = ''
+        for i in $(seq 1 ${toString pkgs.globals.nbInstancesPerRelay}); do
+          ip -6 address add ::127.0.0.$i/96 dev lo || true
+        done
+      '';
       services.cardano-node = {
+        # Options to enable p2p relays in mixed topology cluster:
+
+        # To ensure non-systemd socket activated instances bind the same port on the machine, ie: 3001,
+        # This ensures they all receive incoming traffic.
+        # Since systemd sockets are not used, there is no so_reuseport socket UID conflict.
+        # For non-mingw32 hosts, node enables so_reuseport for socket configuration by default.
+        shareIpv4port = true;
+
+        # The typical ipv4 legacy cluster topology uses systemd socket activation with an ipv6
+        # localhost listener of ::1 with different port binding to enable intra-machine node peering.
+        # Without systemd socket activation, node cli only parameterizes a single port option that is used for both ipv4 and ipv6.
+        # Enabling this option will ensure topology port declaration for intra-machine peering uses the same port.
+        # This infers, though, that the ipv6 addresses for each instance on a machine will need to be different.
+        shareIpv6port = lib.mkForce true;
+
+        # Per above, we wish to increment the ipv6 address for each instance to create a unique intramachine node listener.
+        shareIpv6Address = false;
+
+        # Turn systemd socket activation off due to an so_reuseport UID kernel conflict when binding sockets for re-use as non-root user.
+        systemdSocketActivation = lib.mkForce false;
+
+        # Use p2p
         useNewTopology = true;
+
+        # Transform any p2p multi-member accessPoints groups into single member accessPoints.
         useSingleMemberAccessPoints = true;
+
+        # Make 3rd party producers localRoots rather than publicRoots for a 1:1 equivalency with legacy topology.
         useInstancePublicProducersAsProducers = true;
+
+        # Don't use any chain source outside of declared localRoots
         usePeersFromLedgerAfterSlot = -1;
       };
     } ["rel-a-1"])
