88a17c939446ad6f051973f10bcc7fb730a45dfe
John Lotoski <john.lotoski@iohk.io>
2023-06-23 15:05:25 -0500

    imp: enable p2p topo on select mn relays

--- modules/base-service.nix
+++ modules/base-service.nix
@@ -26,15 +26,27 @@ let
     ip = staticRouteIp nodeName;
   }) deployedProducers;
 
-  toNormalizedProducerGroup = producers: {
-    accessPoints = map (n: {
+  toNormalizedProducerGroup = producers: let
+    mkAccessPointsElement = n: {
       address = let a = n.addr or n; in if (nodes ? ${a}) then hostName a else a;
       port = n.port or nodePort;
     } // lib.optionalAttrs (!cfg.useNewTopology) {
       valency = n.valency or 1;
+    };
+
+    mkAccessPoints = producers: {
+      accessPoints = map (n: mkAccessPointsElement n) producers;
+      valency = length producers;
+    };
+
+    mkSingleMemberAccessPoints = producers: map (n: {
+      accessPoints = [(mkAccessPointsElement n)];
+      valency = n.valency or 1;
     }) producers;
-    valency = length producers;
-  };
+  in if cfg.useSingleMemberAccessPoints then
+    mkSingleMemberAccessPoints producers
+  else
+    mkAccessPoints producers;
 
   producerShare = i: producers: let
       indexed = imap0 (idx: node: { inherit idx node;}) producers;
@@ -45,19 +57,26 @@ let
     cfg.maxIntraInstancesPeers
     (genList (i: {name = i;}) cfg.instances);
 
-  instanceProducers = i: map toNormalizedProducerGroup (filter (g: length g != 0) [
+  instanceProducers = i: (lib.flatten (map toNormalizedProducerGroup (filter (g: length g != 0) [
       (concatMap (i: map (p: {
         addr = cfg.ipv6HostAddr p;
-        port = cfg.port + p;
+        port = if cfg.shareIpv6port
+          then cfg.port
+          else cfg.port + p;
       }) i.producers) (filter (x: x.name == i) intraInstancesTopologies))
       (producerShare i sameRegionRelays)
       (producerShare (cfg.instances - i - 1) otherRegionRelays)
       (producerShare i coreNodeProducers)
-    ]);
+    ]))) ++ lib.optionals cfg.useInstancePublicProducersAsProducers (
+      lib.flatten (map toNormalizedProducerGroup (filter (g: length g != 0) [
+        (producerShare (cfg.instances - i - 1) thirdParyProducers)
+      ]))
+    );
 
-  instancePublicProducers = i: map toNormalizedProducerGroup (filter (g: length g != 0) [
-    (producerShare (cfg.instances - i - 1) thirdParyProducers)
-  ]);
+  instancePublicProducers = i: lib.optionals (!cfg.useInstancePublicProducersAsProducers)
+    (lib.flatten (map toNormalizedProducerGroup (filter (g: length g != 0) [
+      (producerShare (cfg.instances - i - 1) thirdParyProducers)
+    ])));
 
 in
 {
@@ -70,22 +89,54 @@ in
   options = {
     services.cardano-node = {
       publicIp = mkOption { type = types.str; default = staticRouteIp name;};
+
       allProducers = mkOption {
         default = [];
         type = types.listOf (types.either types.str types.attrs);
         description = ''Static routes to peers.'';
       };
+
       totalMaxHeapSizeMbytes = mkOption {
         type = types.float;
         default = config.node.memory * 1024 * 0.790;
       };
+
       totalCpuCores = mkOption {
         type = types.int;
         default = min config.node.cpus (2 * cfg.instances);
       };
+
       maxIntraInstancesPeers = mkOption {
         type = types.int;
         default = 5;
+      };
+
+      shareIpv6Address = mkOption {
+        type = types.bool;
+        default = true;
+        description = ''
+          Should instances on same machine share ipv6 address.
+          Default: true, sets ipv6HostAddr equal to ::1.
+          If false use address increments starting from instance index + 1.
+        '';
+      };
+
+      useSingleMemberAccessPoints = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          If set true with useNewTopology enabled, a p2p localRoots or publicRoots
+          element with n target members in accessPoints will instead be written as n
+          localRoots or publicRoots elements each with 1 target member in accessPoints.
+        '';
+      };
+
+      useInstancePublicProducersAsProducers = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          If set true with useNewTopology enabled, p2p publicRoots will become localRoots.
+        '';
       };
     };
   };
@@ -124,7 +175,10 @@ in
       environment = globals.environmentName;
       cardanoNodePackages = lib.mkDefault cardanoNodePackages;
       inherit hostAddr nodeId instanceProducers instancePublicProducers;
-      ipv6HostAddr = mkIf (cfg.instances > 1) "::1";
+      ipv6HostAddr = mkIf (cfg.instances > 1) (
+        if cfg.shareIpv6Address then "::1"
+        else (i: "::127.0.0.${toString (i + 1)}")
+      );
       producers = mkDefault [];
       publicProducers = mkDefault [];
       port = nodePort;
