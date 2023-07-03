22bf890e1acef8f1006222c32cc41b36f1892c4d
Michael Fellinger <michael.fellinger@iohk.io>
2023-07-03 10:46:42 +0200

    fix shellcheck issues for resize script

--- scripts/resize-ebs-disks.sh
+++ scripts/resize-ebs-disks.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env bash
 
 set -euo pipefail
@@ -6,31 +5,27 @@ set -euo pipefail
 USAGE="Usage: $0 TARGET_SIZE_GB node1 [node2 ... nodeN]"
 
 if [ $# -lt 2 ]; then
-	echo "$USAGE"
-	exit 1
+  echo "$USAGE"
+  exit 1
 fi
 
 TARGET_SIZE="$1"; shift
-
-TARGET_NODES="";
-
-while (( "$#" )); do
-    TARGET_NODES="$TARGET_NODES $1"
-    shift
-done
+TARGET_NODES=("$@")
 
 cd "$(dirname "$0")/.."
 
-DEPLOY_JSON=$(nixops export -d $NIXOPS_DEPLOYMENT)
-DEPLOY_ID=$(jq -r 'keys | .[]' <<< $DEPLOY_JSON)
+DEPLOY_JSON=$(nixops export -d "$NIXOPS_DEPLOYMENT")
 
-for r in $TARGET_NODES; do
-    AWS_PROFILE=$(jq -r ".\"$DEPLOY_ID\".resources.\"$r\".\"ec2.accessKeyId\"" <<< $DEPLOY_JSON)
-    REGION=$(jq -r ".\"$DEPLOY_ID\".resources.\"$r\".\"ec2.region\"" <<< $DEPLOY_JSON)
-    VOL_ID=$( (jq -r ".\"$DEPLOY_ID\".resources.\"$r\".\"ec2.blockDeviceMapping\"" | jq -r '."/dev/xvda".volumeId') <<< $DEPLOY_JSON)
-    echo "resizing root volume for $r"
+for r in "${TARGET_NODES[@]}"; do
+    AWS_PROFILE=$(jq -r ".[].resources.\"$r\".\"ec2.accessKeyId\"" <<< "$DEPLOY_JSON")
+    REGION=$(jq -r ".[].resources.\"$r\".\"ec2.region\"" <<< "$DEPLOY_JSON")
+    VOL_ID=$( (jq -r ".[].resources.\"$r\".\"ec2.blockDeviceMapping\"" | jq -r ".\"/dev/xvda\".volumeId") <<< "$DEPLOY_JSON")
+    echo "resizing root volume for $r (profile: $AWS_PROFILE region: $REGION volume: $VOL_ID)"
     export AWS_PROFILE
-    aws --region $REGION ec2 modify-volume --size $TARGET_SIZE --volume-id $VOL_ID
+    aws --region "$REGION" ec2 modify-volume --size "$TARGET_SIZE" --volume-id "$VOL_ID"
  done
 
-nixops ssh-for-each -p --include $TARGET_NODES -- 'nix-shell -p cloud-utils --run "growpart /dev/xvda 1 && resize2fs /dev/disk/by-label/nixos"'
+nixops ssh-for-each -p --include "${TARGET_NODES[@]}" -- '
+  nix-shell -p cloud-utils --run "
+    (growpart /dev/xvda 1 || growpart /dev/nvme0n1 2) && resize2fs /dev/disk/by-label/nixos"
+'
