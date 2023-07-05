8d2e07952b47e4b4cc4eabaa46564fa75aada37d
John Lotoski <john.lotoski@iohk.io>
2023-07-05 16:37:05 -0500

    imp: inc relay to 20% p2p

--- topologies/mainnet.nix
+++ topologies/mainnet.nix
@@ -174,12 +174,12 @@ let
       };
     } (lib.flatten [
       # See the nixops deploy [--build-only] [--include ...] trace for calculated p2p percentages per region.
-      (p2pRelayRegionList "a" 4) # Currently 40 total region a relays, each represents 2.5% of region total
-      (p2pRelayRegionList "b" 3) # Currently 25 total region b relays, each represents 4.0% of region total
-      (p2pRelayRegionList "c" 1) # Currently 10 total region c relays, each represents 10.0% of region total
-      (p2pRelayRegionList "d" 2) # Currently 15 total region d relays, each represents 6.67% of region total
-      (p2pRelayRegionList "e" 2) # Currently 15 total region e relays, each represents 6.67% of region total
-      (p2pRelayRegionList "f" 1) # Currently 10 total region f relays, each represents 10.0% of region total
+      (p2pRelayRegionList "a" 8) # Currently 40 total region a relays, each represents 2.5% of region total
+      (p2pRelayRegionList "b" 5) # Currently 25 total region b relays, each represents 4.0% of region total
+      (p2pRelayRegionList "c" 2) # Currently 10 total region c relays, each represents 10.0% of region total
+      (p2pRelayRegionList "d" 3) # Currently 15 total region d relays, each represents 6.67% of region total
+      (p2pRelayRegionList "e" 3) # Currently 15 total region e relays, each represents 6.67% of region total
+      (p2pRelayRegionList "f" 2) # Currently 10 total region f relays, each represents 10.0% of region total
     ]))
   ]) (
     map (withModule {
